c6a80284aba506c69f04b0c5c7695087fb73b554
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2021-06-13 17:30:32 -0300

    Fixing some works.

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
-  name = 'flaskstarter',
-  packages = ['flaskstarter'],
-  version = '0.1',
+  name='flaskstarter',
+  packages=['flaskstarter'],
+  version='0.1.1',
   license='apache-2.0',
-  description = 'A Flask project start-up CLI to create a modular ready projects.',
-  author = 'Felipe Bastos Nunes',
-  author_email = 'felipe.bastosn@gmail.com',
-  url = 'https://github.com/felipebastos/flaskstart',
-  download_url = 'https://github.com/felipebastos/flaskstart/archive/refs/tags/v0.1.tar.gz',
-  keywords = ['flask', 'cli', 'project'],
-  install_requires=[
-          'click',
-      ],
+  description='A Flask project start-up CLI to create a modular ready projects.',
+  author='Felipe Bastos Nunes',
+  author_email='felipe.bastosn@gmail.com',
+  url='https://github.com/felipebastos/flaskstart',
+  download_url='https://github.com/felipebastos/flaskstart/archive/refs/tags/v0.1.tar.gz',
+  keywords=['flask', 'cli', 'project'],
+  install_requires=['click',],
   classifiers=[
     'Development Status :: 3 - Alpha',      #"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'Intended Audience :: Developers',
